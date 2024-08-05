ee878ee2b968702c86fa36e0a405791ed7d48f97
shakuyakusystem <30tinjuu8@gmail.com>
2024-08-06 08:58:50 +0900

    レビューバリデーション実装

--- src/app/Models/Store.php
+++ src/app/Models/Store.php
@@ -24,8 +24,8 @@ class Store extends Model
     public function favorites(){
         return $this->hasMany('App\Models\Favorite');
     }
-    public function store_reviews(){
-        return $this->hasMany('App\Models\Store_review');
+    public function reviews(){
+        return $this->hasMany('App\Models\review');
     }
     
     
--- src/database/migrations/2024_07_31_091042_create_reviews_table.php
+++ src/database/migrations/2024_07_31_091042_create_reviews_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateStoreReviewsTable extends Migration
+class CreateReviewsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateStoreReviewsTable extends Migration
      */
     public function up()
     {
-        Schema::create('storeReviews', function (Blueprint $table) {
+        Schema::create('reviews', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('store_id');
@@ -30,6 +30,6 @@ class CreateStoreReviewsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('store_reviews');
+        Schema::dropIfExists('reviews');
     }
 }
